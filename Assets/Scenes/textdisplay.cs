a3813631d9127e43b2dab880fb0649d260b5cfdd
yaminhtetoo <78915140+yaminhtetoo@users.noreply.github.com>
2021-03-20 23:11:03 +0800

    added Scenes in Unity

---
+++ Assets/Scenes/textdisplay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+ 
+public class textdisplay : MonoBehaviour {
+
+    public Text Textfield;
+
+    public void SetText(string text)
+    {
+        Textfield.text = text;
+    }
+}
