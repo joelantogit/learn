3a818f4191c9f0580821eae3ed44c1a6a64eb54f
michelle <hninnandarag@gmail.com>
2021-04-05 22:46:36 +0800

    added new scenes
    new scenes - world, challenge

---
+++ Assets/Scenes/challenge.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class challenge : MonoBehaviour
+{
+    // Start is called before the first frame update
+    List <string> playerlist = new List<string>(); //list of users
+    int worldnum = 2;
+    string selectedplayer ;
+
+    void Start()
+    {
+        getPlayerList(worldnum);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void onSelect(string player)
+    {
+        selectedplayer = player;
+    }
+
+    void inviteplayer()
+    {
+        //send notification to selected player
+    }
+
+    void getPlayerList(int worldnum)
+    {
+
+    }
+}
