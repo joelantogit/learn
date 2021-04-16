8f02692e07e72757454bbe4f02db23b61e26a9aa
Phone5mm <phonemyintthumm@gmail.com>
2021-04-17 05:36:28 +0800

    added dummy quiz

---
+++ Assets/Scripts/controller/AnswerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public bool isCorrect = false;
+    public QuizManager quizManager;
+    public void Answers()
+    {
+        if (isCorrect)
+        {
+            Debug.Log("Correct Answer");
+            quizManager.correct();
+        }
+        else
+        {
+            Debug.Log("Wrong Answer");
+            quizManager.correct();
+        }
+    }
+}
