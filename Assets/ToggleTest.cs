c40cf9200124bb29b33a369f78b341f7706a0227
heintaiksi <78535273+heintaiksi@users.noreply.github.com>
2021-04-27 16:47:32 +0800

    Create classroom

---
+++ Assets/ToggleTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Linq;
+
+public class ToggleTest : MonoBehaviour
+{
+    ToggleGroup toggleGroupInstance;
+    public Toggle currentSelection
+    {
+        get { return toggleGroupInstance.ActiveToggles().FirstOrDefault(); }
+    }
+
+    void start ()
+    {
+        toggleGroupInstance = GetComponent<ToggleGroup>();
+        Debug.Log("First Selected" + currentSelection.name);
+        SelectToggle(2);
+    }
+
+    public void SelectToggle(int id)
+    {
+        var toggles = toggleGroupInstance.GetComponentsInChildren<Toggle>();
+        toggles[id].isOn = true;
+    }
+}
